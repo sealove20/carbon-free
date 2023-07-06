04c63c9355d0324f12af97a6adc50e79c434fa72
Matheus Henrique <matheus.181137@gmail.com>
2023-07-06 16:56:52 -0300

    feat: create button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,20 @@
 import {Text, TouchableOpacity} from './button.styles';
+import {type RuleSet} from 'styled-components/native/dist/types';
 
 interface IButton {
   title: string;
   onPress: () => void;
   disabled?: boolean;
+  customStyle?: RuleSet;
 }
 
-export function Button({title, onPress, disabled}: IButton) {
+export function Button({title, onPress, disabled, customStyle}: IButton) {
   return (
-    <TouchableOpacity onPress={onPress} disabled={disabled}>
+    <TouchableOpacity
+      onPress={onPress}
+      disabled={disabled}
+      customStyle={customStyle}
+    >
       <Text>{title}</Text>
     </TouchableOpacity>
   );
--- src/components/Button/button.styles.ts
+++ src/components/Button/button.styles.ts
@@ -1,11 +1,12 @@
-import { styled, css } from 'styled-components/native';
+import {styled, css} from 'styled-components/native';
+import {type RuleSet} from 'styled-components/native/dist/types';
 
 interface TouchableOpacityProps {
-  customStyle?: string;
+  customStyle?: RuleSet;
 }
 
 export const buttonFont = css`
-  font-family: ${({ theme }) => theme.fonts.regular};
+  font-family: ${({theme}) => theme.fonts.regular};
   font-size: 16px;
   letter-spacing: 1px;
   font-weight: bold;
@@ -17,13 +18,13 @@ export const TouchableOpacity = styled.TouchableOpacity<TouchableOpacityProps>`
   justify-content: center;
   flex-direction: row;
   border-radius: 4px;
-  background-color: ${({ theme }) => theme.colors.primary};
+  background-color: ${({theme}) => theme.colors.primary};
   padding: 16px 32px;
   elevation: 8;
   ${(props) => props.customStyle}
 `;
 
 export const Text = styled.Text`
-  color: ${({ theme }) => theme.colors.white};
+  color: ${({theme}) => theme.colors.white};
   ${buttonFont}
 `;
