acbb906376624876eb7c0055e54908f384c87aee
Matheus Henrique <matheus.181137@gmail.com>
2023-07-08 15:46:07 -0300

    feat: use min-height to keep button aspect in small sizes

--- src/components/Header/header.styles.ts
+++ src/components/Header/header.styles.ts
@@ -1,7 +1,7 @@
 import {styled} from 'styled-components/native';
 
 export const HeaderContainer = styled.View`
-  height: 100px;
+  min-height: 100px;
   border-bottom-color: ${({theme}) => theme.colors.lightGray};
   border-bottom-width: 1px;
 `;
