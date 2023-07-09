66d5ff49dc068dce33b755084d1f5ce3a04d3c35
Matheus Henrique <matheus.181137@gmail.com>
2023-07-09 13:22:45 -0300

    fix: earn reward component alignment

--- src/features/home/components/EarRewards/earRewards.styles.ts
+++ src/features/home/components/EarRewards/earRewards.styles.ts
@@ -4,9 +4,8 @@ export const EarnRewardsContainer = styled.View`
   background-color: ${({theme}) => theme.colors.lightPurple};
   flex-direction: row;
   align-items: center;
-  width: 110px;
   border-radius: 4px;
-  padding: 8px;
+  padding: 6px;
 `;
 
 export const earnText = css`
